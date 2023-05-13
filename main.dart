e3a857eed20d3f822da4f120f419724f078aadf0
Ramin105 <rmnselimli@gmail.com>
2023-05-13 17:19:47 +0400

    update

---
+++ main.dart
@@ -0,0 +1,10 @@
+main() {
+   Set<int> set1 = {150,200,500,170,200} ;
+   print(set1) ; 
+    List<int> list1 = [150, 200, 500, 170,];
+   print(list1[1]);
+   list1.sort(); // Listeyi artan sıraya göre sırala
+  for (int i = 0; i < list1.length; i++) {
+    print(list1[i]);}
+
+}
